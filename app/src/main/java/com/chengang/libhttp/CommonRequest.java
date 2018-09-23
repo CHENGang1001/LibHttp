53da65a4680c196a927c38f85d318f34e1d86370
chengang <cumtcg@126.com>
2018-09-23 23:41:17 +0800

    add():封装了请求参数和请求体

---
+++ app/src/main/java/com/chengang/libhttp/CommonRequest.java
@@ -0,0 +1,49 @@
+package com.chengang.libhttp;
+
+import java.util.Map;
+
+import okhttp3.FormBody;
+import okhttp3.Request;
+
+/**
+ * Created by chengang on 2018/9/23.
+ */
+
+public class CommonRequest {
+
+    /**
+     * get
+     * @param url
+     * @param params
+     * @return
+     */
+    public static Request getRequest(String url ,RequestParams params){
+        StringBuilder builder = new StringBuilder(url).append("?");
+        if (params!=null){
+            for (Map.Entry<String,String> entry :params.urlHashMap.entrySet()){
+                builder .append(entry.getKey())
+                        .append("=")
+                        .append(entry.getValue())
+                        .append("&");
+            }
+        }
+        return new Request.Builder().url(builder.substring(0,builder.length()-1)).get().build();
+    }
+
+    /**
+     * post FormBody
+     * @param url
+     * @param params
+     * @return
+     */
+    public static Request createPostRequest(String url ,RequestParams params){
+        FormBody.Builder  builder =new  FormBody.Builder();
+        if (params!=null){
+            for (Map.Entry<String,String>entry:params.urlHashMap.entrySet()){
+                builder.add(entry.getKey(),entry.getValue());
+            }
+        }
+        FormBody body = builder.build();
+        return new Request.Builder().post(body).url(url).build();
+    }
+}
---
+++ app/src/main/java/com/chengang/libhttp/RequestParams.java
@@ -0,0 +1,61 @@
+package com.chengang.libhttp;
+
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by chengang on 2018/9/23.
+ */
+
+public class RequestParams {
+    public ConcurrentHashMap<String ,String> urlHashMap = new ConcurrentHashMap<>();
+    public ConcurrentHashMap<String ,Object> fileHashMap = new ConcurrentHashMap<>();
+
+    /**
+     * Constructs null
+     */
+    public RequestParams() {
+        this((Map<String,String>) null);
+    }
+
+    /**
+     * Constructs
+     * @param stringStringMap
+     */
+    public RequestParams(Map<String, String> stringStringMap) {
+        if (stringStringMap!=null){
+            for (Map.Entry<String,String> source :stringStringMap.entrySet()){
+                put(source.getKey(),source.getValue());
+            }
+        }
+    }
+
+    public RequestParams(final String key, final String value) {
+        this(new HashMap<String ,String>(){
+            {
+                put(key,value);
+            }
+        });
+    }
+
+    public void put(String key, String value) {
+        if (key!=null&&value!=null){
+            urlHashMap.put(key,value);
+        }
+    }
+
+    public void put(String key,Object object)throws FileNotFoundException{
+        if (key!=null&&object!=null){
+            fileHashMap.put(key,object);
+        }
+    }
+
+    public boolean isHasParams(){
+        if (urlHashMap.size()>0||fileHashMap.size()>0){
+            return true;
+        }
+        return false;
+    }
+}
